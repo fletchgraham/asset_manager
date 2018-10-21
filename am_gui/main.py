c9bc05e07790f4e127290d6a00fd475800841cea
fletchgraham <sfletchgraham@gmail.com>
2018-10-21 15:02:03 -0400

    separate creation and styling in asset list class

--- am_gui/main.py
+++ am_gui/main.py
@@ -11,23 +11,26 @@ class AssetList(Frame):
     def __init__(self, parent=None):
         Frame.__init__(self, parent)
 
-        Label(self, text="Assets:").grid(row=0, column=0, sticky=W)
-        Button(self, text="Refresh", command=self.refresh_tree).grid(row=0, column=1, sticky=E)
-
+        # Create the Elements:
+        l = Label(self, text="Assets:")
+        b = Button(self, text="Refresh", command=self.refresh_tree)
         self.tree = Treeview(self)
-        self.tree.grid(row=1, column=0, columnspan=2, sticky=N+E+S+W)
 
+        # Style and place the elements:
+        l.grid(row=0, column=0, sticky=W)
+        b.grid(row=0, column=1, sticky=E)
+        self.tree.grid(row=1, column=0, columnspan=2, sticky=N+E+S+W)
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
         self.grid_rowconfigure(1, weight=1)
 
     def refresh_tree(self):
+        """Clear the tree then re-crawl for assets."""
         prefs = Prefs()
         try:
             assets = crawl(prefs.get('root_folder'))
         except:
             assets = ['check your prefs']
-
         self.tree.delete(*self.tree.get_children())
         for i in assets:
             self.tree.insert('', 'end', text=i)
