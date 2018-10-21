babd879178bba7772ff0cce87e03d72bd6b54114
fletchgraham <sfletchgraham@gmail.com>
2018-10-21 14:54:21 -0400

    finally get the gui layout on the right track

--- am_gui/main.py
+++ am_gui/main.py
@@ -6,28 +6,55 @@ from tkinter import *
 from tkinter.ttk import *
 from am_gui.settings import SettingsDialog
 
+class AssetList(Frame):
+    """Gui element for getting settings from the user."""
+    def __init__(self, parent=None):
+        Frame.__init__(self, parent)
+
+        Label(self, text="Assets:").grid(row=0, column=0, sticky=W)
+        Button(self, text="Refresh", command=self.refresh_tree).grid(row=0, column=1, sticky=E)
+
+        self.tree = Treeview(self)
+        self.tree.grid(row=1, column=0, columnspan=2, sticky=N+E+S+W)
+
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure(1, weight=1)
+
+    def refresh_tree(self):
+        prefs = Prefs()
+        try:
+            assets = crawl(prefs.get('root_folder'))
+        except:
+            assets = ['check your prefs']
+
+        self.tree.delete(*self.tree.get_children())
+        for i in assets:
+            self.tree.insert('', 'end', text=i)
+
+class ActionsMenu(Frame):
+    """Gui element for getting settings from the user."""
+    def __init__(self, parent=None):
+        Frame.__init__(self, parent)
+        Button(self, text="Preferences", command=show_settings).grid(row=0)
+        Button(self, text="Exit", command=sys.exit).grid(row=1)
+
+        self.grid_columnconfigure(0, weight=1)
 def show_settings():
     popup = Toplevel()
     SettingsDialog(popup).grid(row=0, column=0, sticky=E)
 
+# MAIN APP:
+
 root = Tk()
 
-prefs = Prefs()
+paned_window = Panedwindow(root, orient=HORIZONTAL)
+paned_window.pack(fill=BOTH, expand=True)
 
-Label(text="Assets:").grid(row=0, column=0)
-Button(text="Refresh").grid(row=0, column=2)
+treeview = AssetList(paned_window)
+menu = ActionsMenu(paned_window)
 
-tree = Treeview()
-tree.grid(row=1, column=0, columnspan=4)
-
-try:
-    assets = crawl(prefs.get('root_folder'))
-except:
-    assets = ['check your prefs']
-for i in assets:
-    tree.insert('', 'end', text=i)
-
-Button(text="Preferences", command=show_settings).grid(row=0, column=5)
-Button(text="Exit", command=sys.exit).grid(row=1, column=5)
+paned_window.add(treeview, weight=4)
+paned_window.add(menu, weight=1)
 
 root.mainloop()
