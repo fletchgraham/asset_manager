6e27640ab310cfa91e4d7ab041304d170fd60f48
fletchgraham <sfletchgraham@gmail.com>
2018-10-23 22:38:48 -0400

    assets in treeview now get little icons!!

--- am_gui/asset_tree.py
+++ am_gui/asset_tree.py
@@ -1,7 +1,11 @@
 """Custom tk treeview class that displys the list of assets."""
 
+from am_utils.prefs import Prefs
+
 from tkinter import *
 from tkinter import ttk
+
+from PIL import Image, ImageTk
 
 import os
 from os import path as p
@@ -13,11 +17,31 @@ class AssetTree(ttk.Treeview):
 
         self['columns'] = ('category', 'library')
         self.refresh(data_model)
+        self.icons = []
 
     def refresh(self, data_model):
+        prefs = Prefs()
+        r = prefs.get('root_folder')
         self.delete(*self.get_children())
+
         for i in data_model:
             head, name = p.split(i)
             head, category = p.split(head)
             head, library = p.split(head)
-            self.insert('', 'end', text=name, values=(category, library))
+
+            # Make icons:
+            thumb_path = p.join(r, '.am', 'thumbs', name + '.jpg')
+            image = Image.open(thumb_path)
+            image.thumbnail((30,30))
+            icon = ImageTk.PhotoImage(image)
+
+            # Icons must be added to a list lest they get garbage collected:
+            self.icons.append(icon)
+
+            self.insert(
+                '',
+                'end',
+                text=name,
+                image=self.icons[-1],
+                values=(category, library)
+                )
